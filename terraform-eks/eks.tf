9074810dbed409f89cec1ac41aef7542e48650a2
sharanyabhamidipati-cmyk <sharanyabhamidipati@gmail.com>
2026-02-27 19:14:29 +0530

    Create eks.tf

---
+++ terraform-eks/eks.tf
@@ -0,0 +1,20 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      max_size     = 2
+      min_size     = 1
+
+      instance_types = ["t3.medium"]
+    }
+  }
+}
